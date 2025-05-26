6603d11b74669e673f1358c5fa465b4beb122c6b
09mehed <foysalalmamun558@gmail.com>
2025-05-26 15:58:42 +0600

    forest-application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl bg-red-500 font-semibold'>Forest Application</h1>
+      <h2 className='text-red-400 text-4xl py-20'>Forest</h2>
+    </>
+  )
+}
+
+export default App
